dc561db8bb01d083b0cb2012f9685891ae384de8
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-04-11 00:38:19 +0700

    Add owner commands

---
+++ commands/owner/log.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+let file = JSON.parse(fs.readFileSync('./assets/log.json', 'utf8'));
+
+exports.run = (bot, message, args) => {
+	if (message.author.id != '303011486916411392') return;
+	if (!args[0]) {
+		return message.channel.send(
+			'Please includes the content to be logged. UwU',
+		);
+	}
+	if (!file) {
+		file = { number: 0 };
+	}
+	const total = file.number + 1;
+	file = { number: total };
+	fs.writeFile('./assets/log.json', JSON.stringify(file), err => {
+		if (err) console.log(err.stack);
+	});
+	const content = args.join(' ');
+	const img = content.split('-img ');
+	const date = new Date();
+	const embed = new Discord.RichEmbed()
+		.setAuthor(`Log #${file.number}`)
+		.addField('Guild:', `${message.guild.name} (${message.guild.id})`, true)
+		.addField(
+			'Channel:',
+			`${message.channel.name} (${message.channel.id})`,
+			true,
+		)
+		.setFooter(
+			`at ${date.getMonth()} / ${date.getDate()} / ${date.getFullYear()}`,
+		);
+	if (args.join(' ').includes('-img')) {
+		embed.setDescription(img[0]);
+		embed.setImage(img[1]);
+	}
+	else {
+		embed.addField('Content', content, true);
+	}
+	bot.channels
+		.get('462167632389341184')
+		.send({ embed })
+		.catch(_ =>
+			message.channel.send('It seems like you\'re not giving valid format!'),
+		);
+	message.channel.send('Logged!');
+};
+
+exports.conf = {
+	aliases: [],
+	cooldown: 1,
+	guildOnly: false,
+	userPerm: [''],
+	botPerm: ['EMBED_LINKS'],
+};
+
+exports.help = {
+	name: 'log',
+	category: 'Owner',
+	description: '',
+	usage: '',
+	param: '',
+};
---
+++ commands/owner/reboot.js
@@ -0,0 +1,21 @@
+exports.run = (bot, message, args) => {
+	// prettier-ignore
+	if (message.author.id != '303011486916411392') return message.channel.send(':question:');
+	process.exit(1);
+};
+
+exports.conf = {
+	aliases: [],
+	cooldown: 0.1,
+	guildOnly: false,
+	userPerm: [''],
+	botPerm: [''],
+};
+
+exports.help = {
+	name: 'reboot',
+	category: 'Owner',
+	description: '**OWNER ONLY**',
+	usage: '',
+	param: '',
+};
