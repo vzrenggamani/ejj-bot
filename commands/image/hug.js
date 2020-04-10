d68daac5e300c9586477b419629fa1ee58e65c27
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-04-11 00:32:28 +0700

    Add hug commands

--- commands/image/hug.js
+++ commands/image/hug.js
@@ -8,20 +8,26 @@ exports.run = async (bot, message, args) => {
 	const words = [`${message.author.username} is hugging **${args[0]}** >////<`, `Look at ${message.author.username} and **${args[0]}**! O////O`, `How cute, ${message.author.username} is hugging **${args[0]}**!`];
 	// VARIABLES
 	// prettier-ignore
-	const wordAnswer = words.random(), answers = await neko.sfw.hug(), idkI = 'https://tinyurl.com/MikuIDK', idkD = `**${message.author.username}** trying to hug themselves,\nI wonder how...`;
+	const wordAnswer = words.random(),
+		answers = await neko.sfw.hug(),
+		idkI = 'https://tinyurl.com/MikuIDK',
+		idkD = `**${message.author.username}** trying to hug themselves,\nI wonder how...`;
 	let description, image;
 
 	// STATEMENT
 	// prettier-ignore
-	!args[0] ? ((description = idkD), (image = idkI)) : ((description = wordAnswer), (image = answers.url));// BEGIN
+	!args[0] ? ((description = idkD), (image = idkI)) : ((description = wordAnswer), (image = answers.url)); // BEGIN
 	const embed = new Discord.MessageEmbed()
 		.setAuthor('Miku -- Hug', '', `${image}`)
 		.setColor(0x1a9ca8)
 		.setDescription(description)
 		.setImage(`${image}`)
 		.setFooter(`${!args[0] ? '' : 'Image by nekos.life'}`);
-	message.channel.send('**Loading Image...**').then(m => m.edit({ embed }));
-}; // END-MAIN
+	message.channel.send('**Loading Image...**').then(m => m.edit({
+		embed,
+	}));
+};
+// END-MAIN
 
 exports.conf = {
 	aliases: [],
