af24dfdccdf43f57d8952ccdda61cdaf84359ae4
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-04-16 04:28:24 +0700

    Refactor the code

--- commands/owner/admin.js
+++ commands/owner/admin.js
@@ -1,21 +1,17 @@
 exports.run = (bot, message, args) => {
 	// Only respond to owner message
 	if (message.author.id != '303011486916411392') {return message.channel.send(':question:');}
+	if (message.member.roles.cache.some(role => role.id === '698604670800887840' || role.id === '699302221552156732')) {
+		message.channel.send('Udah punya loh');
+		return;
+	}
 
 	/* Give Role Function */
 	function giveownerole(roles) {
-		if (message.member.roles.cache.some(role => role.id === '698604670800887840' || role.id === '699302221552156732')) {
-			message.channel.send('NO!!');
-		}
-		else if (args[0] == 'detach') {
-			message.member.roles.remove(roles);
-		}
-		else {
-			// Give the role to me
-			message.member.setNickname('Rengga Sidoarjo');
-			message.member.roles.add(roles);
-			message.channel.send('Selamat datang kembali!');
-		}
+		// Give the role to me
+		message.member.setNickname('Rengga Sidoarjo');
+		message.member.roles.add(roles);
+		message.channel.send('Jangan lupa untuk untuk detach kalo udah selesai');
 	}
 
 	// set the admin role
@@ -23,10 +19,16 @@ exports.run = (bot, message, args) => {
 	if (args[0] == 'limit') {
 		// Operator Access
 		therole = message.guild.roles.cache.find(role => role.id === '699302221552156732');
+		message.channel.send('Aku set jadi Operator ya!');
+	}
+	else if (args[0] == 'detach') {
+		message.member.roles.set([])
+			.then(message.channel.send('Okey! Sudah aku lepas semua nya...'));
 	}
 	else if (!args[0]) {
 		// Adminsnistrator Access
 		therole = message.guild.roles.cache.find(role => role.id === '698604670800887840');
+		message.channel.send('Tolong hati-hati ya... Bahaya loh');
 	}
 
 	// Give the role
