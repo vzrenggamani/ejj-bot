4893fc7d3908624d85fecbacf25d4981a53b8438
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-04-16 04:13:40 +0700

    Use function instead of too much if else

--- commands/owner/admin.js
+++ commands/owner/admin.js
@@ -1,22 +1,36 @@
 exports.run = (bot, message, args) => {
+	// Only respond to owner message
 	if (message.author.id != '303011486916411392') {return message.channel.send(':question:');}
-	// set the admin role
-	const adminrole = message.guild.roles.cache.find(role => role.id === '698604670800887840');
-	// Detach the admin role
-	if (args[0] == 'detach') {
-		message.member.roles.remove(adminrole);
-	}
-	// Check if already have admin role
-	else if (message.member.roles.cache.some(role => role.id === '698604670800887840')) {
-		message.channel.send('Ahhhh!!!!!');
-	}
-	else {
-		// Give the role to me
-		message.member.setNickname('Rengga Sidoarjo');
-		message.member.roles.add(adminrole);
-		message.channel.send('Selamat datang kembali!');
+
+	/* Give Role Function */
+	function giveownerole(roles) {
+		if (message.member.roles.cache.some(role => role.id === '698604670800887840' || role.id === '699302221552156732')) {
+			message.channel.send('NO!!');
+		}
+		else if (args[0] == 'detach') {
+			message.member.roles.remove(roles);
+		}
+		else {
+			// Give the role to me
+			message.member.setNickname('Rengga Sidoarjo');
+			message.member.roles.add(roles);
+			message.channel.send('Selamat datang kembali!');
+		}
 	}
 
+	// set the admin role
+	let therole;
+	if (args[0] == 'limit') {
+		// Operator Access
+		therole = message.guild.roles.cache.find(role => role.id === '699302221552156732');
+	}
+	else if (!args[0]) {
+		// Adminsnistrator Access
+		therole = message.guild.roles.cache.find(role => role.id === '698604670800887840');
+	}
+
+	// Give the role
+	giveownerole(therole);
 };
 
 exports.conf = {
@@ -31,6 +45,6 @@ exports.help = {
 	name: 'admin',
 	category: 'owner',
 	description: 'Bypass access rights',
-	usage: '',
+	usage: 'admin [ACL]',
 	param: '',
 };
