674dd74fb856f25afcc5de9d898505f2691220d4
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-04-16 22:39:32 +0700

    Add glitch update

---
+++ glitch-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo STARTING UPDATE DATA
+echo ============================================
+echo repository : gl://vzrenggamani/ejj-bot.git
+echo mirror     : gh://vzrenggamani/ejj-bot.git
+echo Author     : Rengga Prakoso Nugroho
+echo ============================================
+
+echo Git Pulling from origin
+git pull --no-edit
+
+echo Refreshing the Glitch Editor
+refresh
+
+exit
